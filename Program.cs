7adbb3d1a08969f5bd3195f10af055735d500c2f
Lindsay <Lindsay@DESKTOP-MRT9GBF>
2021-10-10 20:56:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace From_A_to_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] number = Console.ReadLine().Split(" ");
+            int a = int.Parse(number[0]);
+            int b = int.Parse(number[1]);
+            int x = 0;
+            if (((1 > a) || (a > (1000000000))) || ((1 > b) || (b > (1000000000))))
+            {
+                return;
+            }
+            while (a > b)
+            {
+                if (a % 2 == 0)
+                {
+                    a /= 2;
+                }
+                else
+                {
+                    a++;
+                }
+                x++;
+            }
+            while (a < b)
+            {
+                a++;
+                x++;
+            }
+
+            Console.WriteLine(x);
+        }
+    }
+}
